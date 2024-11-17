5c7dbcb1da39179090f6f4b6251714bf8c5c6ff0
DevanshGoyal29 <devanshgoyal224@gmail.com>
2024-11-17 19:12:53 +0530

    Add files via upload

---
+++ start.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let start = document.querySelector("#start");
+    start.addEventListener("click", () => {
+        window.location.href = "fruits.html";
+    });
+});
